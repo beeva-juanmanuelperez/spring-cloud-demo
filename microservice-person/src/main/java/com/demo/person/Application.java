73f6ddd9359fb95964cc01a8fd20f17bc01090e8
Juan Manuel Pérez Rodríguez <juanmanuel.perez@beeva.com>
2015-11-02 20:11:02 +0100

    Implemented a remote call with Ribbon in person microservice

--- microservice-person/src/main/java/com/demo/person/Application.java
+++ microservice-person/src/main/java/com/demo/person/Application.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 
@@ -24,11 +26,21 @@ public class Application
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
+    /**
+     * This instance of RestTemplate is annotated with @LoadBalanced, so it will automatically
+     * use Ribbon to find microservices registered in Eureka directly from its id
+     */
+    @Autowired @LoadBalanced
+    RestTemplate restTemplate;
+
     @Autowired
     private Person person;
 
     @Value("${server.port}")
     private int port;
+
+    @Value("${microservice.uuid-generator}")
+    String invokedMicroservice;
 
     @Autowired
     private PersonConfigurationProperties properties;
@@ -37,11 +49,7 @@ public class Application
     public String home(@RequestParam(required = false) String text) {
         String domain = "unknown";
 
-        try {
-            domain = InetAddress.getLocalHost().getHostAddress();
-        } catch (Exception e) {
-            // Do nothing
-        }
+        String results = restTemplate.getForObject("http://" + invokedMicroservice, String.class);
 
         LOGGER.info("This person is " + person);
 
@@ -49,7 +57,8 @@ public class Application
                 person.getName() + " " +
                 person.getSurname() + "</h1>" +
                 "<p><strong>Age</strong> " + person.getAge() + " years old</p>" +
-                "<p><strong>Location</strong> " + person.getLocation() + "</p>"
+                "<p><strong>Location</strong> " + person.getLocation() + "</p>" +
+                "<p><strong>Request id</strong> " + results + "</p>"
         ;
     }
 	
