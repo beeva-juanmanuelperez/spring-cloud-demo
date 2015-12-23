b00fb40e3516d42abd995d3b14cee8724011ac7f
Juan Manuel Pérez Rodríguez <juanmanuel.perez@beeva.com>
2015-12-23 16:48:50 +0100

    Fixed author tag

--- microservice-person/src/main/java/com/demo/person/config/PersonConfiguration.java
+++ microservice-person/src/main/java/com/demo/person/config/PersonConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @author BBVA Open Platform Development Team
+ * @author Juan Manuel Pérez Rodríguez
  */
 @Configuration
 public class PersonConfiguration {
