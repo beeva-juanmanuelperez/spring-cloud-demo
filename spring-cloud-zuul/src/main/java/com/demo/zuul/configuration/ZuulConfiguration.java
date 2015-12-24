133004f7a8ebe55ef1485716f41f7a80e5ea2a1c
Juan Manuel Pérez Rodríguez <juanmanuel.perez@beeva.com>
2015-12-24 11:47:28 +0100

    Removing unnecesary configurations

--- spring-cloud-zuul/src/main/java/com/demo/zuul/configuration/ZuulConfiguration.java
+++ spring-cloud-zuul/src/main/java/com/demo/zuul/configuration/ZuulConfiguration.java
@@ -12,8 +12,6 @@ import org.springframework.context.annotation.Configuration;
 public class ZuulConfiguration {
     /**
      * This RibbonClient overrides the default one. We include a non-default IRule for Load Balancing here.
-     * Care: if included in component scan, this bean is used globally for all ribbon clients. See Spring Cloud docs to
-     * selectively customizing ribbon clients
      *
      * @return
      */
